e5416167701c10ee5da8dea47ef84d31d01409ba
At1llaes22 <at1llaes22@gmail.com>
2024-01-21 18:00:10 +0300

    designed some of the homepage

--- nba-stats-and-predictor/src/home_page/HomePage.js
+++ nba-stats-and-predictor/src/home_page/HomePage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import './HomePage.css';
+
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Welcome to My Homepage</h1>
-            <p>This is the homepage of my first React app.</p>
+        <div className='container'>
+            <div className='topContainer'>
+                <div className='logoContainer'>
+                    <img src="https://picsum.photos/200/300"  />
+                </div>
+                <div className='photoContainer'>
+                    <img src="https://picsum.photos/200/300"  />
+                    <div className='textContainer'>
+                        <h1>Welcome to My Homepage</h1>
+                        <p>This is the homepage of my first React app.</p>
+                    </div>
+                </div>
+            </div>
+            
+                
         </div>
+        
+
+       
     );
 };
 
